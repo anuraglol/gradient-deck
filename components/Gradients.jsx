7a643d31a31936ad57cfbf6cca24c46cae533cbf
Anurag <kr.anurag24@gmail.com>
2021-09-26 18:23:29 +0530

    json

--- components/Gradients.jsx
+++ components/Gradients.jsx
@@ -1,6 +1,19 @@
 import Gradient from "./Gradient";
+import data from "../data.json";
+import { useState } from "react";
 
 export default function Gradients() {
+  const [colors, setColors] = useState([]);
+
+  const fetchData = async () => {
+    console.log(data);
+    data.gradients.map((d) => console.log(d));
+
+    // setColors(data.gradients.map()
+    // console.log(colors);
+  };
+
+  fetchData();
   return (
     <div>
       <Gradient color1="#fafafa" color2="#fff" />
