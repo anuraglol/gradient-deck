c6dab0013fc02d40a35cd663e6096a1d430bdd37
Anurag <kr.anurag24@gmail.com>
2021-09-26 17:20:34 +0530

    changes

--- components/Gradient.jsx
+++ components/Gradient.jsx
@@ -1,6 +1,7 @@
 import { toast, Toaster } from "react-hot-toast";
 import { FiCopy } from "react-icons/fi";
 import copy from "copy-to-clipboard";
+import { Button } from "@mui/material";
 
 export default function Gradient(props) {
   const copyText = () => {
@@ -11,20 +12,22 @@ export default function Gradient(props) {
   };
 
   return (
-    <div
-      className="p-6 grainy w-48 rounded shadow-sm m-4 flex flex-col justify-center cursor-pointer hover:shadow-md"
-      onClick={copyText}
-    >
+    <Button variant="outlined">
       <div
-        className="p-6 rounded"
-        style={{
-          background: `linear-gradient(${props.color1}, ${props.color2})`,
-        }}
-      ></div>
-      <div className="w-full flex justify-center mt-4">
-        <FiCopy size="30" />
+        className="p-6 grainy w-48 rounded shadow-sm m-4 flex flex-col justify-center cursor-pointer hover:shadow-md"
+        onClick={copyText}
+      >
+        <div
+          className="p-6 rounded"
+          style={{
+            background: `linear-gradient(${props.color1}, ${props.color2})`,
+          }}
+        ></div>
+        <div className="w-full flex justify-center mt-4">
+          <FiCopy size="30" />
+        </div>
+        <Toaster />
       </div>
-      <Toaster />
-    </div>
+    </Button>
   );
 }
