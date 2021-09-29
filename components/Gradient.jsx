a6a5b8e45aa8153221ab795bf05e2a800b8e1ec6
Anurag <kr.anurag24@gmail.com>
2021-09-29 11:38:47 +0530

    oops

--- components/Gradient.jsx
+++ components/Gradient.jsx
@@ -3,14 +3,15 @@ import { FiCopy } from "react-icons/fi";
 import Button from "@mui/material/Button";
 
 export default function Gradient(props) {
-  const copyText = () => {
-    toast.success("Copied!");
-    navigator.clipboard.writeText(props.code);
-  };
+
+  // const copyText = () => {
+  //   toast.success("Copied!");
+  //   navigator.clipboard.writeText(props.code);
+  // };
 
   return (
     <>
-      <Button>
+      <Button variant="text">
         <div className="m-2 p-4 grainy w-48 h-48 rounded shadow-sm flex flex-col justify-center cursor-pointer hover:shadow-md">
           <div
             className="p-14 rounded"
@@ -18,12 +19,6 @@ export default function Gradient(props) {
               background: `${props.code}`,
             }}
           ></div>
-
-          <div className="flex w-full flex-row items-center justify-center mt-2">
-            <Button className="text-[#020202]" onClick={copyText}>
-              <FiCopy size="25" />
-            </Button>
-          </div>
         </div>
       </Button>
       <Toaster />
