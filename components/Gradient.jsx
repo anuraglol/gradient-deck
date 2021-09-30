32405a9b1cffea8685271fe2adb08458fcbbeeff
Anurag <kr.anurag24@gmail.com>
2021-09-30 10:46:30 +0530

    small-changes

--- components/Gradient.jsx
+++ components/Gradient.jsx
@@ -4,23 +4,28 @@ import Button from "@mui/material/Button";
 
 export default function Gradient(props) {
   const copyText = () => {
-    toast.success("Copied!");
+    // toast.success("Copied!");
     navigator.clipboard.writeText(props.code);
   };
 
   return (
     <>
-      <Button variant="text" onClick={copyText}>
-        <div className=" m-2 p-5 grainy w-52 h-52 rounded shadow-sm flex flex-col justify-center cursor-pointer hover:shadow-md">
-          <div
-            className="p-16 rounded"
-            style={{
-              background: props.code,
-            }}
-          ></div>
-        </div>
-        {/* <Toaster /> */}
-      </Button>
+      <div className="grid justify-center">
+        <Button variant="text" onClick={copyText}>
+          <div className=" m-2 p-5 grainy w-52 h-52 rounded shadow-sm flex flex-col justify-center cursor-pointer hover:shadow-md">
+            <div
+              className="p-16 rounded"
+              style={{
+                background: props.code,
+              }}
+            ></div>
+          </div>
+          {/* <Toaster /> */}
+        </Button>
+        <Button>
+          <FiCopy size="25" style={{ color: "#020202" }} />
+        </Button>
+      </div>
     </>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -7,15 +7,16 @@ export default function Header() {
       <p className="w-44 p-3 bg-[#ffbf24] rounded font-delius font-semibold text-2xl cursor-pointer logo">
         gradient deck
       </p>
-
+      {/* <Auth /> */}
       <p className="w-36 p-4 bg-[#fafafa] cursor-pointer shadow rounded hover:drop-shadow-xl">
-        <a href="https://twitter.com/kr_anurag_" className="flex flex-row items-center">
+        <a
+          href="https://twitter.com/kr_anurag_"
+          className="flex flex-row items-center"
+        >
           <RiTwitterLine size="35" />
           <span className="font-poppins font-medium text-xl">Anurag</span>
         </a>
       </p>
-
-      {/* <Auth /> */}
     </div>
   );
 }
