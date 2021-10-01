6528f29f873a1cbbd0e921b6a1554a604313e9b1
Anurag <kr.anurag24@gmail.com>
2021-10-01 11:36:23 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,10 @@ import { Meta, Gradients, Header } from "../components";
 
 export default function Home() {
   return (
-    <>
+    <div className="h-screen w-screen main">
       <Meta />
       <Header />
       <Gradients />
-    </>
+    </div>
   );
 }
