f6a5f5399a9f4025fc3481f61b1d38f84f2946eb
Anurag <kr.anurag24@gmail.com>
2021-10-02 20:42:17 +0530

    added-google-fonts-links

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -4,14 +4,17 @@ function Meta() {
   return (
     <Head>
       <title>Gradient Deck - Find Awesome Gradients!!</title>
-      <meta name="description" content="" />
+      <meta
+        name="description"
+        content="Gradient-Deck, a place to find curated awesome gradients!!"
+      />
       <link
         rel="shortcut icon"
         href="./assests/favicon.ico"
         type="image/x-icon"
       />
       <link
-        href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+        href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600"
         rel="stylesheet"
       />
 
@@ -19,6 +22,11 @@ function Meta() {
         href="https://fonts.googleapis.com/css?family=Delius+Swash+Caps:regular"
         rel="stylesheet"
       />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@600&display=swap"
+        rel="stylesheet"
+      ></link>
     </Head>
   );
 }
