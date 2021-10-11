890ca202d07a6556fbb5b4e2950dd56066970cd1
Anurag <77309809+kr-anurag@users.noreply.github.com>
2021-10-11 15:18:04 +0000

    made more responsive i guess

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,29 +1,33 @@
 import { RiTwitterLine } from "react-icons/ri";
 import { motion } from "framer-motion";
+import { Tooltip } from '@mui/material'
 
 export default function Header() {
   return (
-    <div className="w-screen header p-6 flex flex-row justify-between items-center h-[6em]">
+    <div className="w-screen header p-4 flex flex-row justify-between items-center h-[6em]">
       {/* logo/name */}
 
       <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-        <p className="w-40 py-2 grid place-items-center bg-[#ffbf24] rounded font-delius font-semibold text-xl cursor-pointer logo md:text-2xl md:p-3 md:w-44">
+        <p className="w-36 py-2 grid place-items-center bg-[#ffbf24] rounded font-delius font-semibold text-xl cursor-pointer logo md:text-2xl md:p-3 md:w-44">
           gradient deck
         </p>
       </motion.div>
 
       {/* twitter link */}
+      <Tooltip title="Connect with me on Twitter">
+     
       <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-        <p className="w-32 p-3 md:w-34  bg-[#fafafa] cursor-pointer shadow rounded hover:drop-shadow-xl md:w-34 ">
+        <p className="mr-1 p-2 md:w-34  bg-[#fafafa] cursor-pointer shadow rounded hover:drop-shadow-xl md:w-34 md:p-3">
           <a
             href="https://twitter.com/kr_anurag_"
             className="flex flex-row items-center justify-center"
           >
-            <RiTwitterLine size="35" />
-            <span className="font-poppins font-medium text-xl">Anurag</span>
+            <RiTwitterLine size="30" />
+            <span className="font-poppins font-medium text-xl hidden sm:inline">Anurag</span>
           </a>
         </p>
       </motion.div>
+       </Tooltip>
     </div>
   );
 }
