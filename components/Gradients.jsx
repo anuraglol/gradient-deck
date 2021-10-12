29fdcdb8da0bc59ff1bbd1f26b9bde884a2d31ba
Anurag <77309809+kr-anurag@users.noreply.github.com>
2021-10-12 06:40:37 +0000

    nothing

--- components/Gradients.jsx
+++ components/Gradients.jsx
@@ -2,6 +2,7 @@ import Gradient from "./Gradient";
 import data from "../data.json";
 
 export default function Gradients() {
+    
   return (
     <div className="mb-4 w-screen mt-14 overflow-hidden grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {data.colors.map((d) => (
