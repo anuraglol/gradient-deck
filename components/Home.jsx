a6c4a767189bb98a5b6c06210fc170baff1605e4
Anurag <77309809+kr-anurag@users.noreply.github.com>
2021-10-12 07:42:19 +0000

    changes

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,7 +2,7 @@ import { AiFillGithub } from 'react-icons/ai'
 
 export default function Home() {
   return (
-    <div className="w-full p-6 bg-[#020202] text-[#3a0ca3] text-center text-xl">
+    <div className="w-full p-6 bg-[#020202] text-white text-center text-xl">
     <div className="font-medium">
      <p className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl">🌈Gradient Deck</p>
      <span className="mt-2 md:text-2xl">Find Awesome Gradients!!</span>
